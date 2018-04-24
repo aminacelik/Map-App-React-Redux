726402add40c32dfe93146d4065dfe205844edb1
Amina Celik <celik.amina@gmail.com>
2018-04-24 19:03:33 +0200

    Added headers

--- node-locations-api/server.js
+++ node-locations-api/server.js
@@ -13,6 +13,12 @@ mongoose.connect('mongodb://localhost/Locationdb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 var routes = require('./api/routes/locationRoutes'); //importing route
 routes(app); //register the route
